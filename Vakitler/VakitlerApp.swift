59a8cd5ce28034efbe944964d19c42c879db2c67
gulalicelik <gulalicelikmi@gmail.com>
2023-03-11 11:57:50 +0300

    Initial Commit

---
+++ Vakitler/VakitlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VakitlerApp.swift
+//  Vakitler
+//
+//  Created by Gülali Çelik on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct VakitlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
