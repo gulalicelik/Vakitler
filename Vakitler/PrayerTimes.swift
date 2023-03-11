5d331d9d488699134c8eb209a7a1e3fd652095ed
gulalicelik <gulalicelikmi@gmail.com>
2023-03-11 12:48:47 +0300

    add default enums

--- Vakitler/PrayerTimes.swift
+++ Vakitler/PrayerTimes.swift
@@ -12,6 +12,24 @@ enum PrayerTimes: String {
     case sun
     case dhuhr
     case asr
-    case maghtib
+    case maghrib
     case isha
+    
+    var displayName: String {
+        switch self {
+    
+        case .fajr:
+            return NSLocalizedString("PRAYER_DISPLAY_FAJR", comment: "Fajr time display name")
+        case .sun:
+            return NSLocalizedString("PRAYER_DISPLAY_SUN", comment: "Sun time display name")
+        case .dhuhr:
+            return NSLocalizedString("PRAYER_DISPLAY_DHUHR", comment: "Dhuhr time display name")
+        case .asr:
+            return NSLocalizedString("PRAYER_DISPLAY_ASR", comment: "Asr time display name")
+        case .maghrib:
+            return NSLocalizedString("PRAYER_DISPLAY_MAGHRIB", comment: "Maghrib time display name")
+        case .isha:
+            return NSLocalizedString("PRAYER_DISPLAY_ISHA", comment: "Isha time display name")
+        }
+    }
 }
